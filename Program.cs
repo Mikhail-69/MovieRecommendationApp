7b5bb3768196635cccb92efe5c4cb966921a9efb
Mikhail-69 <ivanovmikhail29012007@gmail.com>
2026-03-28 00:10:46 +0300

    Disable HTTPS redirection for Render

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ internal class Program
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseAuthorization();
         app.MapControllers();
